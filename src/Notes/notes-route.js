53ae19c982148b4b70be66a44cc4a8bde04d84e1
maleckim <maleckimatthewm@gmail.com>
2020-04-01 22:03:19 -0500

    added some note routes

--- src/Notes/notes-route.js
+++ src/Notes/notes-route.js
@@ -21,5 +21,43 @@ NotesRouter
     .catch(next)
 
 })
+.delete( jsonBodyParser, (req, res, next) => {
+  const data = req.body;
+
+  NotesService.deleteNote(req.app.get('db'), data)
+    .then( del => {
+      res.status(204).end();
+    })
+
+
+})
+
+NotesRouter
+.route('/add')
+.post( jsonBodyParser, (req, res, next) => {
+
+  const {user_id, subject, topic, content} = req.body
+
+  NotesService.addNote(req.app.get('db'), user_id, subject, topic, content)
+    .then( added => {
+      res.status(200).end()
+    })
+    .catch(next)
+
+})
+
+NotesRouter
+  .route('/edit')
+  .post( jsonBodyParser, (req, res, next) => {
+
+    const data = req.body;
+    const { id } = req.body;
+    
+
+    NotesService.editNote(req.app.get('db'), id, data)
+      .then( edit => {
+        res.status(200).end();
+      })
+  })
 
 module.exports = NotesRouter
--- src/Notes/notes-service.js
+++ src/Notes/notes-service.js
@@ -2,11 +2,44 @@ const NotesService = {
 
   getAllNotes(db, id){
     return db('user_notes')
-    .where({user_id:`${id}`})    
+      .where({user_id:`${id}`})    
 },
+
+  addNote(db, user_id, subject, topic, content){
+    return db('user_notes')
+      .insert([
+        {user_id: `${user_id}`,
+        subject: `${subject}`,
+        topic: `${topic}`,
+        content: `${content}`}
+      ])
+},
+  editNote(db, id, data){
+    const { note_id, subject, topic, content } = data;
+  
+    return db('user_notes')
+      .where('user_id',`${id}`)
+      .andWhere('id', `${note_id}`)
+      .update({
+        subject: `${subject}`,
+        topic: `${topic}`,
+        content: `${content}`
+    })
+      
+},
+
+  deleteNote(db, data){
+    const{user_id, note_id} = data;
+    return db('user_notes')
+      .where('user_id',`${user_id}`)
+      .andWhere('id', `${note_id}`)
+      .del()
+  }
   
 
 
 }
 
 module.exports = NotesService
+
+
